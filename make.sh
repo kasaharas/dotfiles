fdb52d9766355971b912e661c19ca12a0b363a12
sylviah@andrew.cmu.edu <curiouskasahara@gmail.com>
2013-05-04 21:13:45 -0400

    Added script to make files into dotfiles and symlink

---
+++ make.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/.dotfiles # dotfiles directory
+files="bashrc vimrc vim tmux.conf" # list of files/folders to symlink in homedir
+
+##########
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create sy
+#mlinks from the homedir to any files in the ~/dotfiles directory specified in $f# iles
+for file in $files; do
+  rm ~/.$file
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
+done
